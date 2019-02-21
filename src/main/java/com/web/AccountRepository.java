a2a2f753a968a6384370f8d7dc1254ee1f0a6d70
Anil Mehrotra <M1046461@mindtree.com>
2019-02-21 06:55:51 +0530

    client for account api

---
+++ src/main/java/com/web/AccountRepository.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.web;
+
+import java.util.List;
+
+public interface AccountRepository {
+	
+	List<Account> getAllAccounts();
+	
+	Account getAccount(String number);
+}
---
+++ src/main/java/com/web/RemoteAccountRepository.java
@@ -0,0 +1,44 @@
+package com.web;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.client.RestTemplate;
+
+public class RemoteAccountRepository implements AccountRepository {
+	
+	@Autowired
+	protected RestTemplate restTemplate;
+	
+	protected String serviceUrl;
+	
+	public RemoteAccountRepository(String serviceUrl) {
+		this.serviceUrl = serviceUrl.startsWith("http") ? serviceUrl
+				: "http://" + serviceUrl;
+	}
+	
+	@Override
+	public List<Account> getAllAccounts() {
+		Account[] accounts = restTemplate.getForObject(serviceUrl+"/accounts", Account[].class);
+		return Arrays.asList(accounts);
+	}
+	
+	@Override
+	public Account getAccount(String number) {
+		return restTemplate.getForObject(serviceUrl + "/accounts/{id}",
+				Account.class, number);
+	}
+	
+	/*@Override
+    public List getAllAccounts() {
+    Application application = eurekaClient.getApplication(ACCOUNTS_SERVICE_URL);
+    InstanceInfo instanceInfo = application.getInstances().get(0);
+    this.serviceUrl = “http://” + instanceInfo.getIPAddr() + “:” + instanceInfo.getPort()
+
+    Account[] accounts = restTemplate.getForObject(serviceUrl+”/accounts”, Account[].class);
+    return Arrays.asList(accounts);
+    }*/
+
+
+}
